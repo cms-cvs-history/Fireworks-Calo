c620ee6800ca150e8fb639c735e2a39cf03963c3
Salvatore Rappoccio <sha1-15b661ca0cbf207b4ae3d8fa8842a4216051970d@cern.ch>
2008-06-17 17:54:55 +0000

    Added pirated glimpse view branch where we pirate the 3-d lego view for the time being.

--- src/CaloJetProxyEveLegoBuilder.cc
+++ src/CaloJetProxyEveLegoBuilder.cc
@@ -8,7 +8,7 @@
 //
 // Original Author:  
 //         Created:  Sun Jan  6 23:57:00 EST 2008
-// $Id: CaloJetProxyEveLegoBuilder.cc,v 1.3 2008/06/05 14:17:24 dmytro Exp $
+// $Id: CaloJetProxyEveLegoBuilder.cc,v 1.4 2008/06/09 19:54:03 chrjones Exp $
 //
 
 // system include files
@@ -20,6 +20,7 @@
 #include "TEveGeoNode.h"
 #include "TROOT.h"
 #include "TEveStraightLineSet.h"
+#include "TEveCompound.h"
 
 // user include files
 #include "Fireworks/Calo/interface/CaloJetProxyEveLegoBuilder.h"
@@ -87,31 +88,23 @@ CaloJetProxyEveLegoBuilder::build(const FWEventItem* iItem, TEveElementList** pr
    }
    
    fw::NamedCounter counter("jet");
-   TColor* c = gROOT->GetColor( tList->GetMainColor() );
-   Float_t rgba[4] = { 1, 0, 0, 1 };
-   if (c) {
-      rgba[0] = c->GetRed();
-      rgba[1] = c->GetGreen();
-      rgba[2] = c->GetBlue();
-   }
 
-   const unsigned int nLineSegments = 20;
-   const double jetRadius = 0.5;
    for(reco::CaloJetCollection::const_iterator jet = jets->begin(); 
        jet != jets->end(); ++jet, ++counter) {
-      TEveStraightLineSet* container = new TEveStraightLineSet( counter.str().c_str() );
-      // container->SetLineWidth(4);
-      container->SetLineColor(  iItem->defaultDisplayProperties().color() );
+
+      TEveCompound* container = new TEveCompound( counter.str().c_str() );
+      container->OpenCompound();
+      //guarantees that CloseCompound will be called no matter what happens
+      boost::shared_ptr<TEveCompound> sentry(container,boost::mem_fn(&TEveCompound::CloseCompound));
+      	
       
-      for ( unsigned int iphi = 0; iphi < nLineSegments; ++iphi ) {
-	 container->AddLine(jet->eta()+jetRadius*cos(2*M_PI/nLineSegments*iphi),
-			    jet->phi()+jetRadius*sin(2*M_PI/nLineSegments*iphi),
-			    0.1,
-			    jet->eta()+jetRadius*cos(2*M_PI/nLineSegments*(iphi+1)),
-			    jet->phi()+jetRadius*sin(2*M_PI/nLineSegments*(iphi+1)),
-			    0.1);
-      }
+      TEveStraightLineSet* marker = new TEveStraightLineSet("energy");
+      marker->SetLineWidth(1);
+      marker->SetLineColor(  iItem->defaultDisplayProperties().color() );
+      fw::addStraightLineSegment( marker, &*jet );
+      container->AddElement(marker);
       tList->AddElement(container);
+      //container->CloseCompound();
    }
 }
 
