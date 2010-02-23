e903e02b78bbfd833151072a5c9174ec1755a2c0
Ianna Osborne <sha1-f41adf2a6635e62e0a1d2aceba5816a9d25ba935@cern.ch>
2010-02-23 11:02:26 +0000

    Use iSpy-like representation of Hcal rechits

--- plugins/FWHORecHit3DProxyBuilder.cc
+++ plugins/FWHORecHit3DProxyBuilder.cc
@@ -3,6 +3,7 @@
 #include "Fireworks/Core/interface/FWEventItem.h"
 #include "Fireworks/Core/interface/DetIdToMatrix.h"
 #include "DataFormats/HcalRecHit/interface/HORecHit.h"
+#include "TEveBoxSet.h"
 #include "TEveCompound.h"
 #include "TEveStraightLineSet.h"
 
@@ -10,6 +11,7 @@ class FWHORecHit3DProxyBuilder : public FW3DSimpleProxyBuilderTemplate<HORecHit>
 {
 public:
    FWHORecHit3DProxyBuilder(void) 
+     : m_maxEnergy(5.0)
     {}
   
    virtual ~FWHORecHit3DProxyBuilder(void) 
@@ -22,6 +24,8 @@ private:
    const FWHORecHit3DProxyBuilder& operator=(const FWHORecHit3DProxyBuilder&); 	// stop default
 
    void build(const HORecHit& iData, unsigned int iIndex, TEveElement& oItemHolder) const;
+
+   Float_t m_maxEnergy;
 };
 
 void
@@ -32,52 +36,60 @@ FWHORecHit3DProxyBuilder::build(const HORecHit& iData, unsigned int iIndex, TEve
       return;
    }
 
-   Double_t globalFront1Point[3] = {corners[0].fX,  corners[0].fY, corners[0].fZ};
-   Double_t globalFront2Point[3] = {corners[1].fX,  corners[1].fY, corners[1].fZ};
-   Double_t globalFront3Point[3] = {corners[2].fX,  corners[2].fY, corners[2].fZ};
-   Double_t globalFront4Point[3] = {corners[3].fX,  corners[3].fY, corners[3].fZ};
-   Double_t globalBack1Point[3] = {corners[4].fX,  corners[4].fY, corners[4].fZ};
-   Double_t globalBack2Point[3] = {corners[5].fX,  corners[5].fY, corners[5].fZ};
-   Double_t globalBack3Point[3] = {corners[6].fX,  corners[6].fY, corners[6].fZ};
-   Double_t globalBack4Point[3] = {corners[7].fX,  corners[7].fY, corners[7].fZ};
+   TEveVector centre = corners[0] + corners[1] + corners[2] + corners[3] + corners[4] + corners[5] + corners[6] + corners[7];
+   centre.Set(centre.fX / 8.0, centre.fY / 8.0, centre.fZ / 8.0);
 
+   Float_t maxEnergy = m_maxEnergy;
+   Float_t energy = iData.energy();
+   if(energy > maxEnergy)
+   {
+     maxEnergy = energy;
+   }
+   
+   Float_t scaleFraction = energy / maxEnergy;
+   
+   // Coordinates for a scaled version of the original box
+   for(size_t i = 0; i < 8; ++i)
+      corners[i] = centre + (corners[i]-centre)*scaleFraction;
+
+   const Float_t box[8*3] = {corners[0].fX,  corners[0].fY, corners[0].fZ,
+			     corners[1].fX,  corners[1].fY, corners[1].fZ,
+			     corners[2].fX,  corners[2].fY, corners[2].fZ,
+			     corners[3].fX,  corners[3].fY, corners[3].fZ,
+			     corners[4].fX,  corners[4].fY, corners[4].fZ,
+			     corners[5].fX,  corners[5].fY, corners[5].fZ,
+			     corners[6].fX,  corners[6].fY, corners[6].fZ,
+			     corners[7].fX,  corners[7].fY, corners[7].fZ};
+   
+   TEveBoxSet* recHit = new TEveBoxSet("HO Rec Hit");
+   recHit->Reset(TEveBoxSet::kBT_FreeBox, kTRUE, 64);
+   recHit->AddBox(box);
+     
    TEveStraightLineSet* rechitSet = new TEveStraightLineSet("HO Rec Hit");
    rechitSet->SetLineWidth(3);
    rechitSet->SetMainColor(item()->defaultDisplayProperties().color());
    rechitSet->SetRnrSelf(item()->defaultDisplayProperties().isVisible());
    rechitSet->SetRnrChildren(item()->defaultDisplayProperties().isVisible());
 
-   Float_t scale = 10.0; 	// FIXME: The scale should be taken form somewhere else
-   Float_t energy = iData.energy();
-   Float_t eScale = scale * energy;
+   for(int j = 0; j < 3; ++j)
+   {
+      rechitSet->AddLine(corners[j].fX,   corners[j].fY,   corners[j].fZ,
+			 corners[j+1].fX, corners[j+1].fY, corners[j+1].fZ);
+      rechitSet->AddLine(corners[j+4].fX, corners[j+4].fY, corners[j+4].fZ,
+			 corners[j+5].fX, corners[j+5].fY, corners[j+5].fZ);
+      rechitSet->AddLine(corners[j].fX,   corners[j].fY,   corners[j].fZ,
+			 corners[j+4].fX, corners[j+4].fY, corners[j+4].fZ);
+   }
+   rechitSet->AddLine(corners[3].fX, corners[3].fY, corners[3].fZ,
+		      corners[7].fX, corners[7].fY, corners[7].fZ);
+   
+   rechitSet->AddLine(corners[3].fX,  corners[3].fY, corners[3].fZ,
+		      corners[0].fX,  corners[0].fY, corners[0].fZ);
 
-   rechitSet->AddLine(globalFront1Point[0], globalFront1Point[1], globalFront1Point[2],
-		      globalFront2Point[0], globalFront2Point[1], globalFront2Point[2]);
-   rechitSet->AddLine(globalFront2Point[0], globalFront2Point[1], globalFront2Point[2],
-		      globalFront3Point[0], globalFront3Point[1], globalFront3Point[2]);
-   rechitSet->AddLine(globalFront3Point[0], globalFront3Point[1], globalFront3Point[2],
-		      globalFront4Point[0], globalFront4Point[1], globalFront4Point[2]);
-   rechitSet->AddLine(globalFront4Point[0], globalFront4Point[1], globalFront4Point[2],
-		      globalFront1Point[0], globalFront1Point[1], globalFront1Point[2]);
+   rechitSet->AddLine(corners[7].fX,  corners[7].fY, corners[7].fZ,
+		      corners[4].fX,  corners[4].fY, corners[4].fZ);
 
-   rechitSet->AddLine(globalBack1Point[0], globalBack1Point[1], globalBack1Point[2],
-		      globalBack2Point[0], globalBack2Point[1], globalBack2Point[2]);
-   rechitSet->AddLine(globalBack2Point[0], globalBack2Point[1], globalBack2Point[2],
-		      globalBack3Point[0], globalBack3Point[1], globalBack3Point[2]);
-   rechitSet->AddLine(globalBack3Point[0], globalBack3Point[1], globalBack3Point[2],
-		      globalBack4Point[0], globalBack4Point[1], globalBack4Point[2]);
-   rechitSet->AddLine(globalBack4Point[0], globalBack4Point[1], globalBack4Point[2],
-		      globalBack1Point[0], globalBack1Point[1], globalBack1Point[2]);
-
-   rechitSet->AddLine(globalFront1Point[0], globalFront1Point[1], globalFront1Point[2],
-		      globalBack1Point[0], globalBack1Point[1], globalBack1Point[2]);
-   rechitSet->AddLine(globalFront2Point[0], globalFront2Point[1], globalFront2Point[2],
-		      globalBack2Point[0], globalBack2Point[1], globalBack2Point[2]);
-   rechitSet->AddLine(globalFront3Point[0], globalFront3Point[1], globalFront3Point[2],
-		      globalBack3Point[0], globalBack3Point[1], globalBack3Point[2]);
-   rechitSet->AddLine(globalFront4Point[0], globalFront4Point[1], globalFront4Point[2],
-		      globalBack4Point[0], globalBack4Point[1], globalBack4Point[2]);     
-
+   rechitSet->AddElement(recHit);
    oItemHolder.AddElement(rechitSet);
 }
 
